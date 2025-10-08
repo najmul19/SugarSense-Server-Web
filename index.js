a42837d99c2c54d92a214b197dd130152cc12129
najmul <mdnajmulislam10992@gmail.com>
2025-10-08 22:38:48 +0600

    prepared for render deploy server

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ let usersColelction;
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     // Database
     const db = client.db("SugerSenseDB");
@@ -39,10 +39,10 @@ async function run() {
     predictionCollection = db.collection("predictions");
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
+    // await client.db("admin").command({ ping: 1 });
+    // console.log(
+    //   "Pinged your deployment. You successfully connected to MongoDB!"
+    // );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
